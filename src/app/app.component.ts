4773333b4679f9d3a9e3089693c403d6118c4f0c
prashanth.mn <prashanth.mn@subex.com>
2021-08-14 21:24:11 +0530

    Share message change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,8 +66,8 @@ export class AppComponent implements OnInit, OnDestroy {
 		let options: ShareOptions ={
 			url: 'https://play.google.com/store/apps/details?id=com.hinid.calender',
 			text: "Hindi Calendar 2021: हमारा कैलेंडर is a simple, pictorial, and informative Hindi calendar app.",
-			title: "Hindi Calendar 2021 : हमारा कैलेंडर",
-			dialogTitle: "Hindi Calendar 2021 : हमारा कैलेंडर"
+			title: "Download : Hindi Calendar 2021 हमारा कैलेंडर \n",
+			dialogTitle: "Download : Hindi Calendar 2021 हमारा कैलेंडर  \n"
 		}
 		Share.share(options);
 	}
